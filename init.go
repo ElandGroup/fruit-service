c40ebb86048c2b491810918a64da594947a9dba1
xiao.xinmiao <xiaoxm_001@13..com>
2017-02-05 11:10:52 +0800

    test code

--- init.go
+++ init.go
@@ -25,9 +25,32 @@ func InitApi(e *echo.Echo) {
 
 	v1 := e.Group("/v1")
 
+	// GET /v1/fruits HTTP/1.1
+	// Host: localhost:1111
 	v1.GET("/fruits", api.Find)
+
 	v1.GET("/fruits/:id", api.Get)
+	// 	POST /v1/fruits HTTP/1.1
+	// Host: localhost:1111
+	// Content-Type: application/json
+	// [{
+	//     "Code":"A12",
+	//     "Price":1,
+	//     "Color":"Red",
+	//     "Name":"Apple",
+	//     "StoreCode":"AA01"
+	// }]
 	v1.POST("/fruits", api.Post)
+	// PUT /v1/fruits HTTP/1.1
+	// Host: localhost:1111
+	// Content-Type: application/json
+	// {
+	//     "Code":"A1",
+	//     "Price":2,
+	//     "Color":"Red",
+	//     "Name":"Apple",
+	//     "StoreCode":"AA02"
+	// }
 	v1.PUT("/fruits", api.Put)
 	v1.PATCH("/fruits", api.Patch)
 
